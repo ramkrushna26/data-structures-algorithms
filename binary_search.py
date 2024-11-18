6f38ba1e282c2f014df597cb304a8984d0f4539e
ramkrushna26 <ramkrushna.bolewar1@gmail.com>
2024-11-18 23:53:50 +0530

    changed

--- binary_search.py
+++ binary_search.py
@@ -22,8 +22,23 @@ def binary_search(arr, num):
     return -1
 
 
+def binary_search_recursive(arr, left, right, num):
+    if left <= right:
+        mid = (left+right) // 2
+        if num == arr[mid]:
+            return mid
+        elif num > arr[mid]:
+            return binary_search_recursive(arr, mid+1, right, num)
+        else:
+            return binary_search_recursive(arr, left, mid-1, num)
+    else:
+        return -1
+
+
 if __name__ == '__main__':
     arr = [1,2,3,4,5,6,7]
-    num = 5
+    num = 6
 
     print(binary_search(arr, num))
+
+    print(binary_search_recursive(arr, 0, len(arr)-1, num))
